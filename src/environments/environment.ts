e883a1e2775572a41906a8c1dbb0029b396c02ad
Amirkhabbax <amir.khabbaz@gmail.com>
2023-08-15 19:39:20 +0330

    Add existing file

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:true,
+    firebase:{
+        apiKey: "AIzaSyABymASX3yflwzZNC5uzinZo4zZ8H15OQo",
+        authDomain: "moshngtraining.firebaseapp.com",
+        projectId: "moshngtraining",
+        storageBucket: "moshngtraining.appspot.com",
+        messagingSenderId: "609408739045",
+        appId: "1:609408739045:web:494afebfc7bb772e86960c",
+        measurementId: "G-90K93ERKQY"
+    }
+};
